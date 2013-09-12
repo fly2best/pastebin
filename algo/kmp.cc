1baeefa983a52195c9c15805fd95902bcb85d6a7
fly2best <fly2best@gmail.com>
2013-09-12 11:12:47 +0800

    sort out the kmp

--- algo/kmp.cc
+++ algo/kmp.cc
@@ -1,84 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-void kmpGetNext(const char* p, int * b);
-void getFail(const char *P, int *f);
 void get_next(const char *p, int *next);
+int kmp(const char *t,const char *p, int *next);
+
 int main(int argc, const char *argv[])
 {
-    int next[256] = {0};
+    char *t = "google";
+    char *p = "gl";
 
-    const char * str = argv[1];
-    int len = strlen(str);
+    int *next = new int(strlen(p));
 
-    kmpGetNext(str, next);
-
-    for (int i = 0; i < len; ++i)
-        printf("%d ", next[i]);
-    printf("\n");
-
-    get_next(str, next);
-
-    for (int i = 0; i < len; ++i)
-        printf("%d ", next[i]);
-    printf("\n");
+    printf("%d\n", kmp(t, p, next));
 
     return 0;
 }
 
-int kmp(const char *T,const char *P, int *f)
+int kmp(const char *t,const char *p, int *next)
 {
-    int n = strlen(T);
-    int m = strlen(P);
+    int n = strlen(t);
+    int m = strlen(p);
 
-    getFail(P,f);
+    int i = 0;
     int j = 0;
 
-    for (int i = 0; i < n; ++i) {
-        while (j != 0 && P[j] != T[i])
-            j = f[j];
+    get_next(p, next);
 
-        if (P[j] == T[i])
-            j++;
+    while (i < n) {
+        while (j >= 0 && p[j] != t[i])
+            j = next[j];
 
-        if ( j == m)
-            return i - m + 1;
+        ++i;
+        ++j;
+
+        if ( j == m )
+            return i - j;
     }
 
     return -1;
 }
-
-
-void getFail(const char *P, int *f)
-{
-    int m = strlen(P);
-    f[0] = f[1] = 0;
-
-    for(int i = 1; i < m; ++i) {
-        int j = f[i];
-        while (j && P[i] != P[j])
-            j = f[j];
-
-        f[i + 1] = P[i] == P[j] ? j + 1 : 0;
-    }
-}
-
-
-void kmpGetNext(const char* p, int * b)
-{
-    int i=0, j=-1;
-    b[i]=j;
-    int m = strlen(p);
-    while (i<m)
-    {
-        while (j>=0 && p[i]!=p[j]) j=b[j];
-        i++; j++;
-        b[i]=j;
-    }
-}
-
-
-
 
 void get_next(const char *p, int *next)
 {
@@ -93,9 +53,53 @@ void get_next(const char *p, int *next)
         while(j >= 0 && p[i] != p[j])
             j = next[j];
 
-        i++; j++;
+        i++; 
+        j++;
         //把next[0] 设置为-1,不是没道理的
         //当next[0]=-1的时候, 可以把代码写的更精简.
         next[i] = j;
     }
 }
+
+// http://chaoswork.com/blog/2011/06/14/kmp%E7%AE%97%E6%B3%95%E5%B0%8F%E7%BB%93/
+// 记得当时参考了这篇文章, 是从wangcong的博客链过去的
+// 当时我对一些细节搞得不是太清楚
+// 今天, 又理了理思路,
+// 
+// 在kmp,算法里重要一点就是next数组的求取
+// next[i] = j 的含义
+// str[0, j) 和 str[i - j, i) 是相等的
+//
+// next[1] = 0 是有意义的意思是说
+// str[0, 0) == str[1, 1]
+// 理解就是 null 等于 null
+// 如果第一个字符不相等就从第0个字符开始比较
+//
+// next[0] = -1 纯粹是个标记, 为-1 是为了编程实现上的简洁
+//
+//
+// 求next数组的过程是一个递归的过程
+// next[i] = j
+// 那么next[i+1] 呢
+// 如果p[i] == p[j]  
+// 则有 str[0, j+1) 等于 str[i-j, i+1)
+// 则next[i+1]=j+1
+// 如果p[i] != p[j], 要递归的走下去
+// 判断p[i] == p[next[j]]
+//
+// 匹配的时候, 如果 j = 0, 还不相等, 那么只加i, j就不动了
+//
+// 时间复杂度分析
+// 这个比较纠结的是
+//
+// 这个循环的执行次数
+//      while (j >= 0 && p[j] != t[i])
+//              j = next[j];
+//
+// 可以从这么分析, j = next[j], j是减小的
+// 但是增加j的位置只有
+//      ++j;
+// 最多增加n次
+// 所以, j = next[j], 最多执行n次
+// 那个while循环的最多执行n次
+// 所以kmp的时间复杂度是O(n)
